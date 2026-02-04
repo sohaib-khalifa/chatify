3fcf04915b463b48dae7bf48c8aa086865af4cb4
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-02-04 12:55:34 +0200

    Add reusable navigation functions pushTo and pushReplacement

---
+++ lib/core/functions/navigation.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void pushReplacement(BuildContext context, Widget newScreen) {
+  Navigator.pushReplacement(
+    context,
+    MaterialPageRoute(builder: (context) => newScreen),
+  );
+}
+
+void pushTo(BuildContext context, Widget newScreen) {
+  Navigator.push(context, MaterialPageRoute(builder: (context) => newScreen));
+}
