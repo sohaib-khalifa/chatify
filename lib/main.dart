a346f49ea97c000624035bb209738cca74b13d01
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-02-04 11:28:16 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ChatifyApp());
+}
+
+class ChatifyApp extends StatelessWidget {
+  const ChatifyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
