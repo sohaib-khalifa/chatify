099eef843a1f13129e8f0de659d1b90b3e50f7d3
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-02-04 13:12:16 +0200

    Create AppColors class with app colors

--- lib/core/styles/app_color.dart
+++ lib/core/styles/app_color.dart
@@ -1,5 +1,14 @@
-class AppColor {
+import 'package:flutter/material.dart';
 
-
-  
+class AppColors {
+  static const Color primary = Color(0xff53B175);
+  static const Color black = Color(0xff181725);
+  static const Color grey = Color(0xff7C7C7C);
+  static const Color lightGrey = Color(0xffEBEBEB);
+  static const Color background = Colors.white;
+  //
+  static const Color secondary = Color(0xffF8A44C); // buttons or highlights
+  static const Color accent = Color(0xff4A90E2); //  Links or side elements
+  static const Color error = Color(0xffFF3B30);
+  static const Color success = Color(0xff27AE60);
 }
