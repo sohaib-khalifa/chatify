2a6a8359d094c54974e0eff886524fd039fe36d7
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-02-04 13:00:18 +0200

    Create AppImages class with constants for image and SVG paths

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,1 +1,6 @@
-class AppImages {}
+class AppImages {
+static const String scholar = 'assets/images/scholar.png';
+  static const String logoSvg = 'assets/images/logo.svg';
+  static const String carrotSvg = 'assets/images/carrot.svg';
+  static const String welcome = 'assets/images/welcome.png';
+}
